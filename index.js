b29429844cccc2c2854dbe52de5bb6abdb49c18d
Benjamin Horsleben <benjamin@fizkerinc.dk>
2013-11-13 12:37:39 +0100

    Added the express-compatible middleware.

---
+++ index.js
@@ -0,0 +1,42 @@
+module.exports = init
+
+var readFile = require('fs').readFile
+var url = require('url')
+
+var moduleMatcher = /(\.min)?\.js$/
+
+function init(path, opts, require) {
+	if(!opts) {
+		require = opts
+		opts = {}
+	}
+
+	if(path.substr(-1) == '/') path = path.substring(0, path.length-1)
+	if(path[0] != '/') path = '/' + path
+	var paramMatcher = new RegExp(path + '/([^/]+)')
+	return function(req, res, next) {
+		if(req.method != 'GET' && req.method != 'HEAD') return next()
+		var requestedPath = url.parse(req.originalUrl).pathname
+		if(requestedPath.indexOf(path) !== 0) return next()
+		var param = requestedPath.match(paramMatcher)
+		if(!param) return next()
+
+		var file = param[1]
+		var moduleName = file.replace(moduleMatcher, '')
+		if(opts.paths && opts.paths[moduleName]) {
+			moduleName = opts.paths[moduleName]
+		}
+
+		var modulePath = require.resolve(moduleName)
+
+		readFile(modulePath, 'utf8', function(err, content) {
+			if(err) return res.send(404)
+
+			if(content[0] == '#') {
+				content = '//' + content
+			}
+			res.set('content-type', 'application/javascript; charset=UTF-8')
+			res.send(200, content)
+		})
+	}
+}
--- readme.md
+++ readme.md
@@ -2,3 +2,14 @@ express-node_modules
 ====================
 
 A simple handler for loading libraries from node_modules to clients
+
+How to use
+----------
+
+Setting it up is easy:
+
+	var module = require('express-node_modules')
+	app.use(module('/lib', require))
+
+From this point on, any url looking like `/lib/some-module` will attempt to load
+`some-module` with the require-function that was handed in.

4ad1b8f93e0bac02cc49203ef88c4ab10b9cbb0b
Benjamin Horsleben <benjamin@fizkerinc.dk>
2014-01-25 19:58:07 +0100

    Added tests for the normal scenario.

--- test/module.js
+++ test/module.js
@@ -1,12 +1,16 @@
 describe('module.js', function() {
+	var fs = require('fs')
 	var module = require('../index')
 	var fakeRequire
 
 	beforeEach(function() {
 		fzkes.reset()
 
+		var resolve = fzkes.fake('require.resolve')
+			.calls(function(moduleName) { return '/path/to/' + moduleName })
+
 		fakeRequire =
-			{ resolve: fzkes.fake('require.resolve')
+			{ resolve: resolve
 			}
 	})
 
@@ -30,11 +34,77 @@ describe('module.js', function() {
 	describe('When calling the module with a path and a require', function() {
 		var middleware
 		beforeEach(function() {
-			middleware = module('/a/b', fakeRequire)
+			middleware = module('/a', fakeRequire)
 		})
 		it('should return a middleware function', function() {
 			middleware.should.be.a('function')
 			middleware.should.have.length(3)
+		})
+
+		describe('and the middleware gets a `GET` request', function() {
+			var req
+			var res
+			var next
+			var i=0
+			beforeEach(function() {
+				i++
+				req =
+					{ method: 'GET'
+					}
+				res =
+					{ send: fzkes.fake('res.send')
+					, set: fzkes.fake('res.set')
+					, bob: i
+					}
+				next = fzkes.fake('next')
+			})
+
+			describe('with a url matching the path', function() {
+				beforeEach(function() {
+					req.originalUrl = '/a/module'
+					middleware(req, res, next)
+				})
+				it('should ask `require.resolve` for the module', function() {
+					fakeRequire.resolve
+						.should.have.been.calledWith('module')
+				})
+				it('should ask `fs.readFile` for the file', function() {
+					fs.readFile
+						.should.have.been.calledWith('/path/to/module')
+				})
+
+				describe('and `fs.readFile` returns a file', function() {
+					beforeEach(function(done) {
+						fs.readFile.callsArg(
+							{ now: true
+							, arguments:
+							  [ null, 'content of file' ]
+							, notify: done
+							}
+						)
+					})
+					it('should return the contents of the file', function() {
+						res.send
+							.should.have.been.calledWith(200, 'content of file')
+					})
+				})
+			})
+
+			describe('with a url that does not match the path', function() {
+				beforeEach(function() {
+					req.originalUrl = '/abc'
+					middleware(req, res, next)
+				})
+				it('should call the `next` function', function() {
+					next.should.have.been.called
+				})
+				it('should not output anything', function() {
+					res.send
+						.should.not.have.been.called
+					res.set
+						.should.not.have.been.called
+				})
+			})
 		})
 	})
 })
